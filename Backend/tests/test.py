ce1f2d0b4756453cb8c31aa1985e405447ef158e
loysoplayer <115035622+loysoplayer@users.noreply.github.com>
2024-12-05 02:52:13 +0300

    Create simple tests
    Write tests, without tests for single e-mail and TypeError or other expected exception

--- Backend/tests/test.py
+++ Backend/tests/test.py
@@ -1,1 +1,100 @@
+import pytest
+from domain.models import User
+from domain.repositories import UserRepositoryInterface
+from repository.database import db
+from repository.migrations import UserModel
+from repository.user_repository import UserRepository
+from repository.utils import hash_password, verify_password
 
+@pytest.mark.parametrize("real_password, entered_password", [('securepassword', 'securepassword'),
+                                                             ('securepassword', 'other_password'),
+                                                             ('126549', '126549'),
+                                                             ('securepassword', '126549')])
+def test_hashing(real_password, entered_password):
+  if real_password == entered_password:
+    assert verify_password(entered_password, hash_password(real_password))
+  else: assert not(verify_password(entered_password, hash_password(real_password)))
+  
+
+
+@pytest.fixture(scope='function')
+def setup_db():
+    # Подключаемся к базе данных
+    db.connect()
+    db.create_tables([User])  # Создаём таблицу User для каждого теста
+  
+    user_repository = UserRepository()
+
+    yield user_repository
+    db.drop_tables([User])
+    db.close()
+  
+
+def test_create_user(setup_db):
+    user_repository = setup_db
+    user = user_repository.create_user("user@example.com", hash_password("securepassword"))
+    
+    # Проверяем, что пользователь был успешно создан
+    assert user.email == "user@example.com"
+    assert verify_password('securepassword', user.hashed_password)
+
+
+def test_get_user_by_email(setup_db):
+    user_repository = setup_db
+    user_repository.create_user("user@example.com", hash_password("securepassword"))
+    
+    # Проверяем, что можем получить пользователя по имени
+    user = user_repository.get_user_by_email()
+    assert user.email == 'user@example.com'
+    assert verify_password('securepassword', user.hashed_password)
+
+def test_get_user_by_id(setup_db):
+    user_repository = setup_db
+    user_repository.create_user("user@example.com", hash_password("securepassword"))
+    
+    # Проверяем, что можем получить пользователя по имени
+    user = user_repository.get_user_by_id(1)
+    assert user.email == 'user@example.com'
+    assert verify_password('securepassword', user.hashed_password)
+
+
+def test_get_all_users(setup_db):
+    user_repository = setup_db
+    user_repository.create_user("user@example.com", hash_password("securepassword"))
+    user_repository.create_user("next@example.com", hash_password("othersecurepassword"))
+
+    users = user_repository.get_all_users()
+    assert len(users) == 2
+    assert users[0].email = 'user@example.com'
+    assert verify_password('securepassword', user[0].hashed_password)
+    assert users[1].email = 'next@example.com'
+    assert verify_password('othersecurepassword', user[1].hashed_password)
+
+
+def test_update_user(setup_db):
+    user_repository = setup_db
+    user_repository.create_user("user1@example.com", hash_password("securepassword"))
+    user_repository.create_user("user2@example.com", hash_password("securepassword"))
+    user_repository.update_user(1, email= "next@example.com")
+    user_repository.update_user(2, hashed_password= hash_password("otherpassword"))
+    user1 = user_repository.get_user_by_id(1)
+    user2 = user_repository.get_user_by_id(2)
+    assert user1.email == "next@example.com"
+    assert verify_password('securepassword', user1.hashed_password)
+    assert user2.email == "user2@example.com"
+    assert verify_password('otherpassword', user2.hashed_password)
+    user_repository.update_user(1, "other@other.com", "anotherpassword")
+    user1 = user_repository.get_user_by_id(1)
+    assert user1.email == "other@other.com"
+    assert verify_password('anotherpassword', user1.hashed_password)
+
+
+def test_delete_user(setup_db):
+    user_repository = setup_db
+    user_repository.create_user("user@example.com", hash_password("securepassword"))
+    user_repository.create_user("next@example.com", hash_password("othersecurepassword"))
+    user_repository.delete_user(1)
+    users = user_repository.get_all_users()
+    assert len(users) == 1
+    assert users[0].email == "next@example.com"
+    
